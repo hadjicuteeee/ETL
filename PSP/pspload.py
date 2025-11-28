3cb14a8ab61af3624f7476ffeaf7d2289b69d7bf
hadjinxx <144666381+hadjicuteeee@users.noreply.github.com>
2025-11-28 23:37:13 +0800

    Update pspload.py

--- PSP/pspload.py
+++ PSP/pspload.py
@@ -3,4 +3,6 @@ from sqlalchemy import create_engine
 def load_data(df):
     engine = create_engine("postgresql+psycopg2://username:password@localhost:5432/project1")
     df.to_sql('psp', con=engine, if_exists='replace', index=False)
+
+    
     print("Success")
